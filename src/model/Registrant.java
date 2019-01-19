87fa10a7c667ef7f71f83c662d8a492d8deb9a26
jefferydecker <jefferydecker@gmail.com>
2019-01-19 17:58:06 -0600

    first commit

---
+++ src/model/Registrant.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.util.Date;
+
+public class Registrant {
+	private String fname;
+	private String lname;
+	private String memberID;
+	private Date CheckinTime;
+	
+	
+}
