a0de75d72802f70578174c2fa5a63537a56cb426
jefferydecker <jefferydecker@gmail.com>
2019-01-19 19:28:11 -0600

    Green Light tests

--- src/model/Registrant.java
+++ src/model/Registrant.java
@@ -3,10 +3,48 @@ package model;
 import java.util.Date;
 
 public class Registrant {
-	private String fname;
-	private String lname;
+	// fields
+	private String name;
 	private String memberID;
 	private Date CheckinTime;
 	
 	
+	public Registrant(String name, String memberID) {
+		super();
+		this.name = name;
+		this.memberID = memberID;
+	}
+
+	public Registrant(String name, String memberID, Date checkinTime) {
+		super();
+		this.name = name;
+		this.memberID = memberID;
+		CheckinTime = checkinTime;
+	}
+
+	// Getters and Setters
+	public String getName() {
+		return name;
+	}
+	
+	public String getMemberID() {
+		return memberID;
+	}
+	
+	public Date getCheckinTime() {
+		return CheckinTime;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setCheckinTime(Date checkinTime) {
+		CheckinTime = checkinTime;
+	}
+	
+	// Helper methods
+	public boolean isCheckedIn() {
+		return getCheckinTime() == null;
+	}
 }
